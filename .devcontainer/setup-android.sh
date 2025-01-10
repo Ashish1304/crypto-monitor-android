f9e6543277701f811d1954d03cfef5cf14d1acae
Ashish1304 <ashishgeorge13@gmail.com>
2025-01-10 07:38:28 -0600

    Create setup-android.sh

---
+++ .devcontainer/setup-android.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install required packages
+sudo apt-get update
+sudo apt-get install -y wget unzip
+
+# Create Android SDK directory
+mkdir -p ${HOME}/android-sdk
+export ANDROID_SDK_ROOT=${HOME}/android-sdk
+
+# Download Android Command Line Tools
+wget https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip
+unzip commandlinetools-linux-*_latest.zip
+mkdir -p ${ANDROID_SDK_ROOT}/cmdline-tools/latest
+mv cmdline-tools/* ${ANDROID_SDK_ROOT}/cmdline-tools/latest/
+rm -r cmdline-tools
+rm commandlinetools-linux-*_latest.zip
+
+# Set up environment variables
+echo "export ANDROID_SDK_ROOT=${ANDROID_SDK_ROOT}" >> ~/.bashrc
+echo "export PATH=${PATH}:${ANDROID_SDK_ROOT}/cmdline-tools/latest/bin:${ANDROID_SDK_ROOT}/platform-tools" >> ~/.bashrc
+
+# Accept licenses and install required packages
+yes | ${ANDROID_SDK_ROOT}/cmdline-tools/latest/bin/sdkmanager --licenses
+${ANDROID_SDK_ROOT}/cmdline-tools/latest/bin/sdkmanager "platform-tools" "platforms;android-33" "build-tools;33.0.2"
